8c3ebb1d5f7e2b996ca6f3ea3d8b5f351cbf3e51
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-12-26 23:58:33 -0300

    finish first class
    * create NestJS projcet
    * setup Prisma ORM
    * create routes and validations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { CreateNotificationBody } from './create-notification-body';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async list(): Promise<unknown> {
+    return this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody): Promise<unknown> {
+    const { recipientId, content, category } = body;
+
+    return await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        recipientId: recipientId,
+        content: content,
+        category: category,
+      },
+    });
   }
 }
